91ed478df7db43532bd57d43636deee75ae73f1e
PipZip <37911683+PipZip@users.noreply.github.com>
2021-07-08 19:16:24 +0600

    remake

--- gulp/tasks/fonts.js
+++
@@ -1,6 +0,0 @@
-module.exports = function () {
-    $.gulp.task('fonts', () => {
-        return $.gulp.src('./src/assets/fonts/**/*.*')
-            .pipe($.gulp.dest('./assets/fonts/'));
-    });
-};
